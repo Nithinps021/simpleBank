54368ba3a11dad403b0a83b5c0597c8c378c4d64
nithinps021 <nithinps021@gmail.com>
2023-01-23 21:39:45 +0530

    added account routes

---
+++ api/account.go
@@ -0,0 +1,73 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/nithinps021/simplebank/db/sqlc"
+)
+
+
+type createAccountRequest struct{
+	Owner    string `json:"owner" binding:"required"`
+	Currency string `json:"currency" binding:"required,oneof=USD EUR" `
+}
+
+func (server *Server) createAccount(ctx *gin.Context){
+	 var req createAccountRequest;
+	 if err:=ctx.ShouldBindJSON(&req); err!=nil{
+		ctx.JSON(http.StatusBadRequest,errorReponse(err))
+		return
+	 }
+	 arg:=db.CreateAccountParams{
+		 Owner: req.Owner,
+		 Currency: req.Currency,
+		 Balance: 0,
+	 }
+	 account,err:= server.store.CreateAccount(ctx,arg)
+	 if(err!=nil){
+		ctx.JSON(http.StatusInternalServerError,errorReponse(err))
+	 }
+	 ctx.JSON(http.StatusOK,account)
+}
+
+type getAccountRequest struct{
+	Id int64  `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context){
+	var params getAccountRequest
+	if err:=ctx.ShouldBindUri(&params); err!=nil{
+		ctx.JSON(http.StatusBadRequest,errorReponse(err))
+	}
+	acccount,err:=server.store.GetAccount(ctx,params.Id)
+	if err!=nil{
+		if err==sql.ErrNoRows{
+			ctx.JSON(http.StatusNotFound,errorReponse(err))
+		}
+		ctx.JSON(http.StatusInternalServerError,errorReponse(err))
+	}
+	ctx.JSON(http.StatusOK,acccount)
+}
+type listAccountsParams struct{
+	PageId int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+func (server *Server) listAccounts(ctx *gin.Context){
+	var req listAccountsParams
+	if err:=ctx.ShouldBindQuery(&req); err!=nil{
+		ctx.JSON(http.StatusInternalServerError,errorReponse(err))
+	}
+	args:=db.ListAccountsParams{
+		Limit: req.PageSize,
+		Offset: (req.PageId-1)*req.PageSize,
+	}
+	fmt.Println(req.PageId,"page_id",req.PageSize," pageSize")
+	accounts,err:=server.store.ListAccounts(ctx,args)
+	if err!=nil{
+		ctx.JSON(http.StatusInternalServerError,errorReponse(err))
+	}
+	ctx.JSON(http.StatusOK,accounts)
+}
